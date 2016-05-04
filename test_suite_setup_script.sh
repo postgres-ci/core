be60d1fbf2f6d18f9963e263ad8284217a8fcded
kshvakov <shvakov@gmail.com>
2016-05-04 13:53:03 +0300

    psql --quiet

--- test_suite_setup_script.sh
+++ test_suite_setup_script.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-env PGPASSWORD=$POSTGRES_PASSWORD psql -q -U $POSTGRES_USER -d $TEST_DATABASE <<-SQL
+env PGPASSWORD=$POSTGRES_PASSWORD psql -v ON_ERROR_STOP=1 -q -U $POSTGRES_USER -d $TEST_DATABASE <<-SQL
 	CREATE SCHEMA postgres_ci;
 	CREATE EXTENSION pgcrypto;
 SQL
 
 cat postgres_ci--0.0.1.sql | sed '/CREATE EXTENSION postgres_ci/d' > dump.sql
 
-env PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -d $TEST_DATABASE -f dump.sql
+env PGPASSWORD=$POSTGRES_PASSWORD psql -v ON_ERROR_STOP=1 -q -U $POSTGRES_USER -d $TEST_DATABASE -f dump.sql
 
 echo "Grant privileges to Tester"
 
-env PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -d $TEST_DATABASE -f tests/grants.sql
+env PGPASSWORD=$POSTGRES_PASSWORD psql -v ON_ERROR_STOP=1 -q -U $POSTGRES_USER -d $TEST_DATABASE -f tests/grants.sql
 
 echo "Create test functions"
 
-cd tests && env PGPASSWORD=$TEST_PASSWORD psql -U $TEST_USERNAME -d $TEST_DATABASE -f tests.sql
+cd tests && env PGPASSWORD=$TEST_PASSWORD psql -v ON_ERROR_STOP=1 -q -U $TEST_USERNAME -d $TEST_DATABASE -f tests.sql
