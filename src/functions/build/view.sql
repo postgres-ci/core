edbe66b2737a7cdb816be636fbb095717c62a452
kshvakov <shvakov@gmail.com>
2016-05-16 15:47:28 +0300

    sync

---
+++ src/functions/build/view.sql
@@ -0,0 +1,98 @@
+create or replace function build.view(
+    _build_id int, 
+
+    out project_id      int,
+    out project_name    text,
+    out branch_id       int,
+    out branch_name     text,
+    out config          text, 
+    out error           text,
+    out status          postgres_ci.status,
+    out commit_sha      text,
+    out commit_message  text,
+    out committed_at    timestamptz,
+    out committer_name  text,
+    out committer_email text,
+    out author_name     text,
+    out author_email    text,
+    out parts           jsonb
+) returns record as $$
+    begin 
+
+        SELECT
+            B.project_id,
+            P.project_name, 
+            B.branch_id, 
+            BR.branch,
+            B.config,
+            B.error,
+            B.status,
+            C.commit_sha,
+            C.commit_message,
+            C.committed_at,
+            C.committer_name,
+            C.committer_email,
+            C.author_name,
+            C.author_email,
+            (
+                SELECT 
+                    COALESCE(array_to_json(array_agg(P.*)), '[]') 
+                FROM (
+                    SELECT 
+                        part_id,
+                        docker_image,
+                        server_version,
+                        output,
+                        success,
+                        started_at,
+                        finished_at,
+                        (
+                            SELECT 
+                                COALESCE(array_to_json(array_agg(T.*)), '[]') 
+                            FROM (
+                                SELECT 
+                                    namespace,
+                                    procedure,
+                                    errors,
+                                    started_at,
+                                    finished_at
+                                FROM postgres_ci.tests 
+                                WHERE part_id = parts.part_id
+                                ORDER BY started_at
+                            ) AS T
+                        ) AS tests
+                    FROM postgres_ci.parts 
+                    WHERE build_id = B.build_id
+                    ORDER BY part_id
+                ) AS P 
+            )
+            INTO 
+                project_id,
+                project_name,
+                branch_id,
+                branch_name,
+                config, 
+                error,
+                status,
+                commit_sha,
+                commit_message,
+                committed_at,
+                committer_name,
+                committer_email,
+                author_name,
+                author_email,
+                parts
+        FROM postgres_ci.builds AS B 
+            JOIN postgres_ci.projects AS P USING(project_id)
+            JOIN postgres_ci.branches AS BR ON BR.branch_id = B.branch_id
+            JOIN postgres_ci.commits  AS C  ON C.commit_id  = B.commit_id
+        WHERE B.build_id = _build_id;
+
+        IF NOT FOUND THEN 
+            SET log_min_messages to LOG;
+
+            RAISE EXCEPTION 'NOT_FOUND' USING ERRCODE = 'no_data_found';
+        END IF;
+    end;
+$$ language plpgsql security definer;
+
